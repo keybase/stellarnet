3adef3d9e1abea8e2071dde87cfe16154308b9e7
Patrick Crosby <pcrosby@gmail.com>
2020-01-06 15:28:45 -0600

    Add NewMemoFromStrings + test

---
+++ memo_test.go
@@ -0,0 +1,79 @@
+package stellarnet
+
+import "testing"
+
+type mtest struct {
+	in   string
+	kind string
+	out  *Memo
+	err  bool
+}
+
+var mtests = []mtest{
+	{
+		in:   "",
+		kind: "none",
+		out:  NewMemoNone(),
+	},
+	{
+		in:   "",
+		kind: "",
+		out:  NewMemoNone(),
+	},
+	{
+		in:   "public memo",
+		kind: "none",
+		err:  true,
+	},
+	{
+		in:   "public memo",
+		kind: "text",
+		out:  NewMemoText("public memo"),
+	},
+	{
+		in:   "18446744073709551615",
+		kind: "id",
+		out:  NewMemoID(18446744073709551615),
+	},
+	{
+		in:   "28446744073709551615",
+		kind: "id",
+		err:  true,
+	},
+	{
+		in:   "0a0b0c0d0e0f",
+		kind: "hash",
+		out:  NewMemoHash(MemoHash{10, 11, 12, 13, 14, 15}),
+	},
+	{
+		in:   "ga0b0c0d0e0f",
+		kind: "hash",
+		err:  true,
+	},
+	{
+		in:   "0a0b0c0d0e0f",
+		kind: "return",
+		out:  NewMemoReturn(MemoHash{10, 11, 12, 13, 14, 15}),
+	},
+}
+
+func TestMemoFromStrings(t *testing.T) {
+	for i, test := range mtests {
+		t.Logf("running test %d: %+v", i, test)
+		m, err := NewMemoFromStrings(test.in, test.kind)
+		if err != nil {
+			if !test.err {
+				t.Errorf("test %d: error %s", i, err)
+			}
+		} else {
+			if test.err {
+				t.Errorf("test %d: expected an error", i)
+				continue
+			}
+			if test.out.String() != m.String() {
+				t.Errorf("test %d: output mismatch expected %s, got %s", i, test.out.String(), m.String())
+			}
+		}
+		t.Logf("test %d: passed", i)
+	}
+}
